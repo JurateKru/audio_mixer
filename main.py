be8e470e227b474aa2b3b742d8e48eb9bec2c5e3
Mantvydas Racickas <mantvydas.racickas@gmail.com>
2023-05-04 11:02:56 +0300

    prideta json, bandomas naujas langas

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 from pygame import mixer
-from time import sleep
 import PySimpleGUI as sg
 import os
+import json
 
 song_list = os.listdir('./audio_samples')  # ['bass.mp3', 'hihat.mps3', 'kick.mp3', 'mr_world.mp3', 'shaker.mp3', 'snap.mp3', 'snare.mp3', 'sound.mp3']
-active_songs = []
-# print(song_list)
-
-song_list_no_ext = []
+song_keys = []
 for song in song_list:
     name, _ = os.path.splitext(song)
-    song_list_no_ext.append(name.upper())
+    song_keys.append(name.upper())
 
-print(song_list_no_ext)
-# exit()
+audio_info = {}
+
+for i, song in enumerate(song_list):
+    name, ext = os.path.splitext(song)
+    audio_info[song_keys[i]] = {
+        'filename': song,
+        'directory': 'audio_samples',
+        'button_name': song_keys[i],
+        'button_location': (i % 4, i // 4) # Assumes a 4x2 layout
+    }
+
+with open('audio_info.json', 'w', encoding="utf-8") as f:
+    json.dump(audio_info, f, indent=4)
+
+with open('audio_info.json', 'r') as f:
+    data = json.load(f)
+
+song_keys = list(data.keys())
 
 def play_sound(channel:int, sound:int):
     channel_n = mixer.Channel(channel)
@@ -24,6 +37,7 @@ def stop_sound(channel:int):
     channel_n = mixer.Channel(channel)
     channel_n.stop()
 
+active_songs = []
 def button(channel, index, event, playing):
     if not playing:
         play_sound(channel, index)
@@ -40,25 +54,32 @@ def song_status(sound):
     else: 
         playing = False
     return playing
-    
-
 
 mixer.init()
 
 layout = [
     [sg.Text('Audio Mixer', font=('Helvetica', 20))],
     [sg.Text('Select an audio to play:', font=('Helvetica', 12))],
-    [sg.Button(song_list_no_ext[0], key=song_list_no_ext[0], button_color=('white', 'black'), size=(10, 3), font=('Helvetica', 14)),
-     sg.Button(song_list_no_ext[1], key=song_list_no_ext[1], button_color=('white', 'black'), size=(10, 3), font=('Helvetica', 14)),
-     sg.Button(song_list_no_ext[2], key=song_list_no_ext[2], button_color=('white', 'black'), size=(10, 3), font=('Helvetica', 14)),
-     sg.Button(song_list_no_ext[3], key=song_list_no_ext[3], button_color=('white', 'black'), size=(10, 3), font=('Helvetica', 14))],
-     [sg.Button(song_list_no_ext[4], key=song_list_no_ext[4], button_color=('white', 'black'), size=(10, 3), font=('Helvetica', 14)),
-     sg.Button(song_list_no_ext[5], key=song_list_no_ext[5], button_color=('white', 'black'), size=(10, 3), font=('Helvetica', 14)),
-     sg.Button(song_list_no_ext[6], key=song_list_no_ext[6], button_color=('white', 'black'), size=(10, 3), font=('Helvetica', 14)),
-     sg.Button(song_list_no_ext[7], key=song_list_no_ext[7], button_color=('white', 'black'), size=(10, 3), font=('Helvetica', 14))],
+    [sg.Button(song_keys[0], key=song_keys[0], button_color=('white', 'black'), size=(10, 3), font=('Helvetica', 14)),
+     sg.Button(song_keys[1], key=song_keys[1], button_color=('white', 'black'), size=(10, 3), font=('Helvetica', 14)),
+     sg.Button(song_keys[2], key=song_keys[2], button_color=('white', 'black'), size=(10, 3), font=('Helvetica', 14)),
+     sg.Button(song_keys[3], key=song_keys[3], button_color=('white', 'black'), size=(10, 3), font=('Helvetica', 14))],
+     [sg.Button(song_keys[4], key=song_keys[4], button_color=('white', 'black'), size=(10, 3), font=('Helvetica', 14)),
+     sg.Button(song_keys[5], key=song_keys[5], button_color=('white', 'black'), size=(10, 3), font=('Helvetica', 14)),
+     sg.Button(song_keys[6], key=song_keys[6], button_color=('white', 'black'), size=(10, 3), font=('Helvetica', 14)),
+     sg.Button(song_keys[7], key=song_keys[7], button_color=('white', 'black'), size=(10, 3), font=('Helvetica', 14))],
+     [sg.Button("Configure", key='testavimas', button_color=('white', 'grey'), size=(26, 1), font=('Helvetica', 12), border_width=3),
+     sg.Button("Edit Me", button_color=('white', 'grey'), size=(26, 1), font=('Helvetica', 12), border_width=3)]
 ]
 
+# TESTAVIMAS NAUJO LANGO
+layout2 = [
+    [sg.Button("BELEKAS", key='testavimas', button_color=('white', 'grey'), size=(26, 1), font=('Helvetica', 12), border_width=3),
+     sg.Button("Edit Me", button_color=('white', 'grey'), size=(26, 1), font=('Helvetica', 12), border_width=3)]
+     ]
+
 window = sg.Window("Audio mixer", layout)
+window2 = sg.Window("Audio mixer", layout2)  # TESTAVIMAS NAUJO LANGO
 
 playing = False
 
@@ -66,28 +87,32 @@ while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
         break
-    elif event == song_list_no_ext[0]:
+    elif event == "testavimas":  # TESTAVIMAS NAUJO LANGO
+        while True:
+            event, values = window2.read()
+            pass
+    elif event == song_keys[0]:
         playing = song_status(event)
         button(0, 0, event, playing)
-    elif event == song_list_no_ext[1]:
+    elif event == song_keys[1]:
         playing = song_status(event)
         button(1, 1, event, playing)
-    elif event == song_list_no_ext[2]:
+    elif event == song_keys[2]:
         playing = song_status(event)
         button(2, 2, event, playing)
-    elif event == song_list_no_ext[3]:
+    elif event == song_keys[3]:
         playing = song_status(event)
         button(3, 3, event, playing)
-    elif event == song_list_no_ext[4]:
+    elif event == song_keys[4]:
         playing = song_status(event)
         button(4, 4, event, playing)
-    elif event == song_list_no_ext[5]:
+    elif event == song_keys[5]:
         playing = song_status(event)
         button(5, 5, event, playing)
-    elif event == song_list_no_ext[6]:
+    elif event == song_keys[6]:
         playing = song_status(event)
         button(6, 6, event, playing)
-    elif event == song_list_no_ext[7]:
+    elif event == song_keys[7]:
         playing = song_status(event)
         button(7, 7, event, playing)
 
