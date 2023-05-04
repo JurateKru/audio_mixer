65a7eb21e2a3b1f946412ea6fa6ae2fb71ad68f8
Evelina Stonyte <stonyteevelina@gmail.com>
2023-05-04 15:46:56 +0300

    Chanels update

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from window2 import configure
 with open('audio_info.json', 'r') as f:
     song_list, song_keys = json.load(f)
 
+
 def play_sound(channel:int, sound:int):
     channel_n = mixer.Channel(channel)
     sound = mixer.Sound(f'audio_samples/{song_keys[sound]}.mp3')
@@ -35,17 +36,21 @@ def song_status(sound):
 
 mixer.init()
 
+all_channels = []
+for index, song in enumerate(song_keys):
+    all_channels.append(index)
+
 layout = [
-[sg.Text('Select an audio to play:', font=('Helvetica', 12))],
-    [sg.Button(song_keys[0], key=song_keys[0], button_color=('white', 'black'), size=(10, 3), font=('Helvetica', 14),border_width=2),
-     sg.Button(song_keys[1], key=song_keys[1], button_color=('white', 'black'), size=(10, 3), font=('Helvetica', 14),border_width=2),
-     sg.Button(song_keys[2], key=song_keys[2], button_color=('white', 'black'), size=(10, 3), font=('Helvetica', 14),border_width=2),
-     sg.Button(song_keys[3], key=song_keys[3], button_color=('white', 'black'), size=(10, 3), font=('Helvetica', 14),border_width=2)],
-     [sg.Button(song_keys[4], key=song_keys[4], button_color=('white', 'black'), size=(10, 3), font=('Helvetica', 14),border_width=2),
-     sg.Button(song_keys[5], key=song_keys[5], button_color=('white', 'black'), size=(10, 3), font=('Helvetica', 14),border_width=2),
-     sg.Button(song_keys[6], key=song_keys[6], button_color=('white', 'black'), size=(10, 3), font=('Helvetica', 14),border_width=2),
-     sg.Button(song_keys[7], key=song_keys[7], button_color=('white', 'black'), size=(10, 3), font=('Helvetica', 14),border_width=2)],
-     [sg.Button("Go to configurator window", key='configure', button_color=('black', 'grey'), size=(55, 1), font=('Helvetica', 12), border_width=3)]
+    [sg.Text('Select an audio to play:', font=('Helvetica', 12))],
+    [sg.Button(song_keys[0], key=all_channels[0], button_color=('white', 'black'), size=(10, 3), font=('Helvetica', 14),border_width=2),
+    sg.Button(song_keys[1], key=all_channels[1], button_color=('white', 'black'), size=(10, 3), font=('Helvetica', 14),border_width=2),
+    sg.Button(song_keys[2], key=all_channels[2], button_color=('white', 'black'), size=(10, 3), font=('Helvetica', 14),border_width=2),
+    sg.Button(song_keys[3], key=all_channels[3], button_color=('white', 'black'), size=(10, 3), font=('Helvetica', 14),border_width=2)],
+    [sg.Button(song_keys[4], key=all_channels[4], button_color=('white', 'black'), size=(10, 3), font=('Helvetica', 14),border_width=2),
+    sg.Button(song_keys[5], key=all_channels[5], button_color=('white', 'black'), size=(10, 3), font=('Helvetica', 14),border_width=2),
+    sg.Button(song_keys[6], key=all_channels[6], button_color=('white', 'black'), size=(10, 3), font=('Helvetica', 14),border_width=2),
+    sg.Button(song_keys[7], key=all_channels[7], button_color=('white', 'black'), size=(10, 3), font=('Helvetica', 14),border_width=2)],
+    [sg.Button("Go to configurator window", key='configure', button_color=('black', 'grey'), size=(55, 1), font=('Helvetica', 12), border_width=3)]
 ]
 
 window = sg.Window("Audio mixer", layout)
@@ -59,31 +64,12 @@ while True:
             json.dump([song_list, song_keys], f)
         break
     elif event == "configure":  # TESTAVIMAS NAUJO LANGO
-        choosen_button, choosen_name = configure(song_keys)
-        song_keys[choosen_button] = choosen_name
-    elif event == song_keys[0]:
-        playing = song_status(event)
-        button(0, 0, event, playing)
-    elif event == song_keys[1]:
-        playing = song_status(event)
-        button(1, 1, event, playing)
-    elif event == song_keys[2]:
-        playing = song_status(event)
-        button(2, 2, event, playing)
-    elif event == song_keys[3]:
-        playing = song_status(event)
-        button(3, 3, event, playing)
-    elif event == song_keys[4]:
-        playing = song_status(event)
-        button(4, 4, event, playing)
-    elif event == song_keys[5]:
-        playing = song_status(event)
-        button(5, 5, event, playing)
-    elif event == song_keys[6]:
-        playing = song_status(event)
-        button(6, 6, event, playing)
-    elif event == song_keys[7]:
-        playing = song_status(event)
-        button(7, 7, event, playing)
+        choosen_channel, choosen_name = configure(all_channels)
+        song_keys[choosen_channel] = choosen_name
+    else:
+        for i in range(8):
+            if event == song_keys[i]:
+                playing = song_status(event)
+                button(i, i, event, playing)
 
 window.close()
